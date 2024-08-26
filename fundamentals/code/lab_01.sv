916b4968be2b012ed091e1829113d37e78f5f930
Mohammad Attari <masoud.ata@gmail.com>
2024-08-26 14:38:55 +0200

    add tasks with solutions to lab 01 (randomization)

--- fundamentals/code/lab_01.sv
+++ fundamentals/code/lab_01.sv
@@ -10,29 +10,34 @@ module lab_01;
 
     initial begin
 
-        randomize_data;
+        randomize_data();
 
-        randomize_my_state;
+        randomize_my_state();
 
-        randomize_data_with_constraints;
+        randomize_data_with_constraints();
 
-        randomize_my_state_with_constraints;
+        randomize_my_state_with_constraints();
 
-        randomize_my_state_with_dist_constraints;
+        randomize_my_state_with_dist_constraints();
 
         my_state = S2;
-        randomize_data_with_conditional_constraints;
+        randomize_data_with_conditional_constraints();
 
         my_state = INIT;
-        randomize_data_with_conditional_constraints;
+        randomize_data_with_conditional_constraints();
 
         // Task 1
+        // Create a function that randomizes my_state 16 times in a way that it excludes START from the randomization
+        task1_solution();
 
         // Task 2
+        // Create a function that randomizes data 16 times in a way that it is twice as likely to get data <= 10 than it is to get data >= 200 (and 0 likelihood for 10 < data < 200)
+        task2_solution();
 
         // Task 3
-
-        // Task 4
+        // Create a function that randomizes data 16 times in a way that if my_state is [S1:S6] then 50 <= data <= 60 or 100 <= data <= 150, otherwise data <= 20
+        my_state = S2;
+        task3_solution();
 
     end
 
@@ -107,6 +112,54 @@ module lab_01;
             result = randomize(data) with { 
                 my_state == INIT -> data < 50; 
                 my_state == S2 -> data > 100; 
+            };
+            $display(data);
+        end
+        $display("");
+    endfunction
+
+
+    function void task1_solution;
+        $display("Randomize 'my_state' for task1");
+        repeat(16) begin
+            result = randomize(my_state) with { 
+                my_state inside {
+                    INIT,
+                    [S1:S6] 
+                }; 
+            };
+            $display(my_state.name);
+        end
+        $display("");
+    endfunction
+
+
+    function void task2_solution;
+        $display("Randomize 'data' for task2");
+        repeat(16) begin
+            result = randomize(data) with { 
+                data dist {
+                    [0:10] :/ 2,
+                    [200:255] :/ 1 
+                };
+            };
+            $display(data);
+        end
+        $display("");
+    endfunction
+
+
+    function void task3_solution;
+        $display("Randomize 'data' for task3");
+        repeat(16) begin
+            result = randomize(data) with { 
+                if (my_state inside {[S1:S6]}) 
+                    data dist {
+                        [50:60] :/ 1,  // 50% chance to be [50:60]
+                        [100:150] :/ 1   // 50% chance to be [100:150]
+                    };                                     
+                else
+                    data < 20; 
             };
             $display(data);
         end
